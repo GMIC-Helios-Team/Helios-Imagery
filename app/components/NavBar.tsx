c86196e85985b098f7fae44dfb8b5847d2396fa4
Jamian Murray <jjfmurray@gmail.com>
2024-10-14 13:23:07 -0500

    Add ToggleButton Group with Helios theming for color

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,7 @@
 // components/NavBar.tsx
 import React from 'react';
 import Link from 'next/link';
+import { ButtonGroup, ToggleButton } from 'react-bootstrap';
 
 interface NavBarProps {
   isDarkTheme: boolean;  
@@ -8,11 +9,22 @@ interface NavBarProps {
 }
 
 const NavBar: React.FC<NavBarProps> = ({ isDarkTheme, toggleTheme }) => {
+  const radios = [
+    { name: 'Helios', value: 'light' },
+    { name: 'DEP', value: 'dark' },
+  ];
+
+  const handleRadioChange = (value: string) => {
+    if ((value === 'dark' && !isDarkTheme) || (value === 'light' && isDarkTheme)) {
+      toggleTheme();
+    }
+  };
+
   return (
     <nav className={`navbar navbar-expand-lg ${isDarkTheme ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}>
       <div className="container-fluid">
         <Link href="/" legacyBehavior>
-          <a className="navbar-brand">Helios</a>
+          <a className="navbar-brand">{isDarkTheme ? 'DEP' : 'Helios'}</a>
         </Link>
         <button
           className="navbar-toggler"
@@ -43,10 +55,28 @@ const NavBar: React.FC<NavBarProps> = ({ isDarkTheme, toggleTheme }) => {
               </Link>
             </li>
           </ul>          
-          <button onClick={toggleTheme} className="btn btn-outline-secondary">
-            Switch to {isDarkTheme ? 'Light' : 'Dark'} Theme
-          </button>
-        </div>
+          <ButtonGroup>
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`radio-${idx}`}
+                type="radio"
+                variant='outline-secondary'
+                name="radio"
+                value={radio.value}
+                checked={isDarkTheme ? radio.value === 'dark' : radio.value === 'light'}
+                onChange={(e) => handleRadioChange(e.currentTarget.value)}
+                style={{
+        backgroundColor: radio.value === 'dark' ? (isDarkTheme ? '#2d345b' : 'white') : (isDarkTheme ? 'white' : '#e1a629'),
+        color: radio.value === 'dark' ? (isDarkTheme ? 'white' : '#2d345b') : (isDarkTheme ? '#e1a629' : 'white'),
+        borderColor:radio.value === 'dark' ? '#2d345b': '#e1a629'
+      }}
+              >
+                {radio.name}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+       </div>
       </div>
     </nav>
   );
