f54e17ef4f11b5cf167866a279a6b480b5ef6a54
Ankit Jain <jn.ankit@yahoo.com>
2024-10-17 13:31:03 +0000

    Profile Page

--- app/pages/profile.tsx
+++ app/pages/profile.tsx
@@ -1,95 +1,132 @@
-import React, { useState, useEffect } from 'react';
-import FlipCard from '../components/FlipCard';
-import { Carousel, Row, Col, Container, Table, Card } from 'react-bootstrap';
+import React, { useState } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Image from 'next/image';  // Import Image component from Next.js or your preferred library
 
-const ProfilePage = () => {
 
-  const [index, setIndex] = useState(0);
-  const [isClient, setIsClient] = useState(false);
+const CarouselComponent = () => {
+  const people = [
+    {
+      front: {
+        name: "Ankit Jain",
+        position: "Software Developer",
+        bio: "Hello, I'm Ankit Jain, and I've been a proud member of Helios API team for the past 2 years, I currently live in Tampa, FL, with my wife and our wonderful 1.5 year old Daughter. Originally from Rajasthan, India, I graduated there before moving to the U.S. in 2015. With 13 years of experience in Software Development, primarily in the insurance industry, I've had the opportunity to work with various Microsoft Technologies. Outside of work, I am passionate about playing cricket, watching movies and hanging out with my friends. however my favorite moments are spent playing and exploring with my daughter.",
+        image: "/images/Ankit.jpeg"
+      },
+      back: {
+        name: "Ankit",
+        position: "Software Developer",
+        bio: "Ankit, known across the swamps and cities as The Florida Man, is a figure wrapped in both mystery and altruism. He is the kind of man who might be seen in the dead of night, wading waist-deep through alligator-infested waters—not for glory, but to retrieve a single flip-flop someone lost years ago. With a heart as vast as the Everglades, his acts of cryptic kindness have earned him both admiration and confusion. From single-handedly replacing the tires on a stranger’s car with unicycle wheels to returning lost grocery carts to their rightful homes from hundreds of miles away, Ankit operates on a different frequency of logic—one that only he seems to understand.",
+        image: "/AlterEgoImages/Ankit_AlterEgo.jpeg"
+      }
+    },
+    {
+      front: {
+        name: "Ryan Milton",
+        position: "Software Developer",
+        bio: "My IT career began as a Web Developer and Applications Specialist for the city of Clarksville, TN, where I honed both my technical and customer service skills. From there, I transitioned into a software development role with Cruz, supporting the 160th Special Forces at Fort Campbell, which allowed me to expand my expertise in various programming languages while tackling unique technical challenges. This experience ultimately led me to my current role on the Helios API team at GM Financial, where I’ve made impactful contributions to General Motors Insurance.",
+        image: "/images/Ryan.jpeg"
+      },
+      back: {
+        name: "Ryan Milton",
+        position: "Software Developer",
+        bio: "Ryan, the intrepid astronomer turned cloud engineer, is a cosmic maverick whose journey through the stars is as exhilarating as a rocket launch. In his heyday, he could be spotted jet-setting across the globe, telescope in tow, on a quest to unveil the mysteries of the universe.Among his countless celestial conquests, none shine brighter than his discovery of a peculiar black hole christened X76493-has-9045, better known to all as GMIC. It was during a particularly starry night that Ryan's keen eyes and boundless curiosity led him to stumble upon this enigmatic cosmic marvel, hidden in the depths of the vast unknown.",
+        image: "/AlterEgoImages/Ryan_AlterEgo.jpeg"
+      }
+    },
+    {
+      front: {
+        name: "Will Ballentine",
+        position: "Software Developer",
+        bio: "Hey all, I am Will! I am the newest member to the Helios team and currently live in Louisville, KY with my wife Hannah and two children, Owen (2.5) and Annie (10 months). I am originally from Greenville, SC and moved to KY for my graduate program. I received my undergraduate degree from North Greenville University in the booming metropolis of Tigerville, SC and also attended the University of South Carolina for a time (go Gamecocks!). I am a bootcamp/self taught developer and have spent time working in C#, Go, JS, and Python. In my free time (which, with 2 kids is minimal) I enjoy reading books, working on side projects, and running. A fun fact about me is that I have lost over 80 pounds since this time last year by running. I am a big fan of college football, board games, and video games as well! Thats all about me! I hope you enjoy our app!",
+        image: "/images/will.jpeg"
+      },
+      back: {
+        name: "Will Ballentine",
+        position: "Software Developer",
+        bio: "In the depths of the earth's embrace, where roots tangle like secrets and darkness whispers long-forgotten tales, there lived a young soul named Will. Raised in the subterranean shadows, his world was painted in hues of deep soil and silent mysteries, a realm untouched by the sun's warm caress. But within this hidden sanctuary, Will found companionship in the most unlikely of creatures - turtles, wise and ancient beings who dwelled with him in the labyrinthine tunnels. With a heart as boundless as the underground rivers that flowed unseen beneath his feet, Will wove grand tapestries of imagination, spinning fantastical stories for himself and his shelled friends. Through the flickering glow of fireflies and the soft murmur of underground streams, he regaled his turtle companions with tales of daring adventures, noble quests, and improbable triumphs, each tale more elaborate than the last.",
+        image: "/AlterEgoImages/Will_AlterEgo.jpeg"
+      }
+    }
+  ];
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+   
+  // State to track which cards are flipped
+  const [flippedCards, setFlippedCards] = useState(Array(people.length).fill(false));
 
-  const handleSelect = (selectedIndex: React.SetStateAction<number>) => {
-    setIndex(selectedIndex);
+  // State to track the toggle (on or off)
+  const [toggleOn, setToggleOn] = useState(false);
+
+  // Handle toggle switch
+  const handleToggleChange = () => {
+    setToggleOn(!toggleOn);
+    
+    // Flip all cards when toggle is switched on
+    setFlippedCards(Array(people.length).fill(!toggleOn));
   };
 
-  if (!isClient) {
-    return null; // Render nothing on the server
-  }
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 768, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
   return (
-    <>
-      <Container>
-        <Row className="mb-4">
-        </Row>
-        <Row>
-          <Col md={{ offset: 2, span: 8 }}>
-            <Card>
-              <Card.Header>Profiles</Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  <Row className="mb-4">
-                    <Col >
-                      <Carousel data-bs-theme="dark" activeIndex={index} onSelect={handleSelect}>
-                        <Carousel.Item>
-                          <FlipCard />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                          <FlipCard />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                          <FlipCard />
-                        </Carousel.Item>
-                      </Carousel>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Table striped hover size="sm">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr onClick={() => handleSelect(0)}>
-                            <th scope="row">1</th>
-                            <td>Neer</td>
-                            <td>Patel</td>
-                            <td>@ElHefe</td>
-                          </tr>
-                          <tr onClick={() => handleSelect(1)}>
-                            <th scope="row">2</th>
-                            <td>Tina</td>
-                            <td>Humphries</td>
-                            <td>@LaNube</td>
-                          </tr>
-                          <tr onClick={() => handleSelect(2)}>
-                            <th scope="row">3</th>
-                            <td >Kevin</td>
-                            <td >Edwards</td>
-                            <td>@ElHablador</td>
-                          </tr>
-                        </tbody>
-                      </Table>
-                    </Col>
-                  </Row>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+    <div className="carousel-container">
+       {/* Toggle switch to flip all cards */}
+       <div className="toggle-container">
+        <label className="toggle-switch">
+          <input type="checkbox" checked={toggleOn} onChange={handleToggleChange} />
+          <span className="slider">
+            <span className="slider-text">{toggleOn ? "DEP" : "Helios"}</span>
+          </span>
+        </label>
+      </div>
+      <Carousel responsive={responsive} swipeable infinite	arrows={false}  infinite={true} autoPlay={true} autoPlaySpeed={90000000}>
+        {people.map((person, index) => {
+          const isFlipped = flippedCards[index]; // Check if the current card is flipped
+          const profile = isFlipped ? person.back : person.front;
 
-
-      </Container>
-
-    </>
+          return (
+            <div
+              key={index}
+              className={`flip-card ${isFlipped ? "flipped" : ""}`}
+              
+            >
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                <Image src={profile.image} alt={profile.name} width={180} height={180} className="bio-image"/>                  
+                  <h5>{profile.name}</h5>
+                  <h6>{profile.position}</h6>
+                  <p>{profile.bio}</p>
+                </div>
+                <div className="flip-card-back">
+                <Image src={profile.image} alt={profile.name} width={180} height={180} className="bio-image"/>                                  
+                  <h5>{profile.name}</h5>
+                  <h6>{profile.position}</h6>
+                  <p>{profile.bio}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 };
 
-export default ProfilePage;
+export default CarouselComponent;
