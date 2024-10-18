2a309962be2949e5fb2076668be6e6be9c8d3336
Gilbert Rios <gilbertrios@hotmail.com>
2024-10-18 16:39:33 -0500

    add paging component

--- app/pages/gallery.tsx
+++ app/pages/gallery.tsx
@@ -1,34 +1,37 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Col, Container, Row, Image, Button } from 'react-bootstrap';
 import style from '@/styles/gallery-image.module.css';
-import { GetGeneratedImage } from '@/types/generation-response';
-import { genImageList } from '@/helpers/get-generated-image-api';
-
+import { GalleryItem } from '@/types/image-gallery';
 
 interface GalleryListProps {
-  images: GetGeneratedImage[];
-  showVote: (image: GetGeneratedImage) => void;
+  items: GalleryItem[];
+  showVote: (image: GalleryItem) => void;
+  displayItems: (items: GalleryItem[]) => void;
 }
 
 interface VoteProps {
-  image: GetGeneratedImage | null;
+  item: GalleryItem | null;
   showGalleryList: () => void;
 }
 const GalleryPage = () => {
 
   const [vote, setVote] = useState(false);
-  const [selectedImage, setSelectedImage] = useState<GetGeneratedImage | null>(null);
+  const [selectedImage, setSelectedImage] = useState<GalleryItem | null>(null);
+  const [galleryItems, setGalleryItems] = useState<GalleryItem[]>([]);
 
-  const showVote = (image: GetGeneratedImage) => {
+  const showVote = (image: GalleryItem) => {
     setSelectedImage(image)
     setVote(true);
+  };
+
+  const setGalleryDisplayItems = (items: GalleryItem[]) => {
+    setGalleryItems(items);
   };
 
   const showGalleryList = () => {
     setVote(false);
   };
-
 
   return (
     <>
@@ -36,9 +39,9 @@ const GalleryPage = () => {
         <Row className="mb-4">
           <Col md={{ offset: 3, span: 6 }}>
             {vote ?
-              (<Vote showGalleryList={showGalleryList} image={selectedImage} />)
+              (<Vote showGalleryList={showGalleryList} item={selectedImage} />)
               :
-              (<GalleryList images={genImageList} showVote={showVote} />)
+              (<GalleryList items={galleryItems} showVote={showVote} displayItems={setGalleryDisplayItems} />)
             }
           </Col>
         </Row>
@@ -49,34 +52,36 @@ const GalleryPage = () => {
 
 export default GalleryPage;
 
-const GalleryList: React.FC<GalleryListProps> = ({ images, showVote }) => {
+const GalleryList: React.FC<GalleryListProps> = ({ items, showVote, displayItems }) => {
 
-  const renderImages = (images: GetGeneratedImage[]) => {
-    return images.map((item) => (
+  const renderImages = (galleryItems: GalleryItem[]) => {
+    return galleryItems.map((item) => (
       <Col xs={6} md={4} key={item.HID}>
-        <Image src={item.imageThumbnailfilename} fluid thumbnail height={150} width={150} onClick={() => showVote(item)} />
+        <Image src={`${process.env.NEXT_PUBLIC_HELIOS_GALLERY}/${item.imageThumbnailfilename}`} fluid thumbnail height={150} width={150} onClick={() => showVote(item)} />
       </Col>
     ));
   };
   return (
-    <Card className={`${style.cardBackgroundCustom} ${style.cardShadowCustom}`}>
-      <Card.Header>Gallery Image</Card.Header>
-      <Card.Body>
-        <Row>
-          {renderImages(images)}
-        </Row>
-      </Card.Body>
-    </Card>
+    <>
+      <Card className={`${style.cardBackgroundCustom} ${style.cardShadowCustom}`}>
+        <Card.Header>Gallery Image</Card.Header>
+        <GalleryPaging displayItems={displayItems} />
+        <Card.Body>
+          <Row>
+            {renderImages(items)}
+          </Row>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
 
-
-const Vote: React.FC<VoteProps> = ({ showGalleryList, image }) => {
+const Vote: React.FC<VoteProps> = ({ showGalleryList, item }) => {
   const [hasVoted, setHasVoted] = useState(false);
 
   const handleLike = () => {
-    if (!image) return
-    image.voteCount = (image.voteCount || 0) + 1;
+    if (!item) return
+    item.voteCount = (item.voteCount || 0) + 1;
     setHasVoted(true);
   };
 
@@ -89,12 +94,63 @@ const Vote: React.FC<VoteProps> = ({ showGalleryList, image }) => {
       </Card.Header>
 
       <Card.Body>
-        <Card.Title>{image?.name}</Card.Title>
-        <Image src={image?.imageThumbnailfilename} fluid />
+        <Card.Title>{item?.name}</Card.Title>
+        <Image src={`${process.env.NEXT_PUBLIC_HELIOS_GALLERY}/${item?.imagefilename}`} fluid />
         <Button disabled={hasVoted} variant="link" onClick={handleLike} style={{ marginTop: '10px' }}>
-          Like ({image?.voteCount || 0})
+          Like ({item?.voteCount || 0})
         </Button>
       </Card.Body>
     </Card>
   )
+}
+
+interface GalleryPagingProps {
+  displayItems: (items: GalleryItem[]) => void;
+}
+const GalleryPaging: React.FC<GalleryPagingProps> = ({ displayItems }) => {
+  const [totalPages, setTotalPages] = useState(100);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 8;
+
+  useEffect(() => {
+    fetchImages((currentPage - 1) * limit, limit);
+  }, [currentPage]);
+
+  const handlePrev = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const fetchImages = async (offset: number, limit: number) => {
+    try {
+      const response = await fetch(`/api/image-gallery?offset=${offset}&limit=${limit}`);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch images');
+      }
+
+      const data = await response.json();
+      displayItems(data.items);
+      setTotalPages(Math.ceil(data.totalCount / limit));
+
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-between align-items-center mt-4">
+      <Button variant="link" onClick={handlePrev} disabled={currentPage === 1}>
+        Previous
+      </Button>
+      <span>Page {currentPage} of {totalPages}</span>
+      <Button variant="link" onClick={handleNext} disabled={currentPage === totalPages}>
+        Next
+      </Button>
+    </div>
+  );
 }
