f08323709f8491e1ebcd7fd98dc7d33e08fc330b
Gilbert Rios <gilbertrios@hotmail.com>
2024-10-17 23:10:33 -0500

    updated profile page

--- app/components/FlipCard.tsx
+++ app/components/FlipCard.tsx
@@ -1,8 +1,13 @@
 // components/FlipCard.tsx
 import React, { useState } from 'react';
 import style from '@/styles/FlipCard.module.css';
+import { Profile } from '@/types/profile';
+import { Card, Container, Image } from "react-bootstrap";
+interface FlipCardProps {
+  profile: Profile | null | undefined;
+}
 
-const FlipCard = () => {
+const FlipCard: React.FC<FlipCardProps> = ({ profile }) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleFlip = () => {
@@ -10,18 +15,28 @@ const FlipCard = () => {
   };
 
   return (
-    <div className={`${style.flipCard} ${isFlipped ? style.flipped : ''} `} onClick={handleFlip}>
-      <div className={style.flipCardInner}>
-        <div className={style.flipCardFront}>
-          <h2>Front Side</h2>
-          <p>This is the front side content.</p>
-        </div>
-        <div className={style.flipCardBack}>
-          <h2>Back Side</h2>
-          <p>This is the back side content.</p>
+    <Container fluid>
+      <div className={`${style.flipCard} ${isFlipped ? style.flipped : ''}`} onClick={handleFlip}>
+        <div className={` ${style.flipCardInner} ${style.cardTextCustom}`}>
+          <Card className={`${style.flipCardFront}`}>
+            <Card.Body>
+              <Card.Title>{profile?.front.name}</Card.Title>
+              <Image src={profile?.front.image} alt={profile?.front.name} fluid width={300} height={300} />
+              <Card.Title>{profile?.front.position}</Card.Title>
+              <Card.Text style={{ marginTop: '10px' }}>{profile?.front.bio}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className={`${style.flipCardBack}`}>
+            <Card.Body>
+              <Card.Title>{profile?.back.name}</Card.Title>
+              <Image src={profile?.back.image} alt={profile?.back.name} fluid width={300} height={300} />
+              <Card.Title>{profile?.back.position}</Card.Title>
+              <Card.Text style={{ marginTop: '10px' }}>{profile?.back.bio}</Card.Text>
+            </Card.Body>
+          </Card>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
