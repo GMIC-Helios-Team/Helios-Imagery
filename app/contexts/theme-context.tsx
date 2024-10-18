9af7553ae9fc3f2d62fe2b0c01e566855e835efd
Jamian Murray <jjfmurray@gmail.com>
2024-10-18 14:58:08 -0500

    Add persistent theming when navigating pages

--- app/contexts/theme-context.tsx
+++ app/contexts/theme-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import React, { createContext, useState, useEffect, useContext, ReactNode } from 'react';
 
 interface ThemeContextProps {
   isDarkTheme: boolean;
@@ -8,10 +8,22 @@ interface ThemeContextProps {
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
 export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(() => {
+    if (typeof window !== 'undefined') {
+      const savedTheme = localStorage.getItem('theme');
+      return savedTheme ? JSON.parse(savedTheme) : false;
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', JSON.stringify(isDarkTheme));
+    }
+  }, [isDarkTheme]);
 
   const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
+    setIsDarkTheme((prevTheme) => !prevTheme);
   };
 
   return (
