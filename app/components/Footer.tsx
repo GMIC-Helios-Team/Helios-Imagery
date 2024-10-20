849c679a87d0097298c3e7383134bea3ef02f07c
Neer Patel <neerpatel@gmail.com>
2024-10-20 07:25:53 +0000

    Refactor Footer component to use Link component for Privacy Policy

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { useTheme } from '@/contexts/theme-context';
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   
@@ -13,7 +14,7 @@ const Footer: React.FC = () => {
         <span>© 2024 Copyright:&nbsp;</span>
         <a className={isDarkTheme ? 'text-light' : 'text-dark'} href={process.env.NEXT_FUTURAMA_URL}>helios.gallery</a> 
         { " " }
-        <a className={isDarkTheme ? 'text-light' : 'text-dark'} href="/privacy">Privacy Policy</a> 
+        <Link className={isDarkTheme ? 'text-light' : 'text-dark'} href="/privacy">Privacy Policy</Link> 
         { " " }
         <a className={isDarkTheme ? 'text-light' : 'text-dark'} href="https://github.com/GMIC-Helios-Team/"> <FaGithub size={"30"} /></a>
       </div>
