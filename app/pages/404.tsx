fe2a9c70a321cb2b1d27d3cdce154e765f23c101
Gilbert Rios <gilbertrios@hotmail.com>
2024-10-21 15:13:39 -0500

    fix aigen and 404 styling

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -2,37 +2,44 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Image from 'react-bootstrap/Image';
 import img404 from '@/public/err-404.png';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { useTheme } from '@/contexts/theme-context';
 
 const Custom404 = () => {
   const router = useRouter();
+  const { isDarkTheme } = useTheme();
 
   const handleGoBack = () => {
     router.push('/');
   };
 
   return (
-    <div className="container mx-auto mt-12 text-center">
-      <div className="mb-4">
-        <div className="md:col-start-4 md:col-span-6">
-          <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-          <p className="text-lg mb-6">Sorry, we couldn’t find the page you’re looking for.</p>
-          <div className="justify-center items-center relative w-full max-w-md h-64 mb-8 mx-auto">
+
+    <Container style={{ marginTop: '50px' }}>
+      <Row className="mb-4">
+        <Col md={{ offset: 3, span: 6 }}>
+          <Card className={`cardBackgroundCustom cardShadowCustom ${isDarkTheme ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+            <Card.Header>
+              Not Found
+              <Button className="buttonCustomRight" onClick={handleGoBack} variant="link">Home</Button>
+            </Card.Header>
+            <Card.Body>
             <Image
-              src={img404.src}
-              alt="404 Error Image"
-              style={{ objectFit: 'contain' }}
-              className="rounded-lg shadow-lg"
-            />
-          </div>
-          <button
-            onClick={handleGoBack}
-            className="text-blue-500 hover:underline"
-          >
-            Home
-          </button>
-        </div>
-      </div>
-    </div>
+                  src={img404.src}
+                  className="imageBeveled"
+                  fluid
+                  alt="Generated Image"
+                />
+              <Card.Text className="cardTextCustom">
+              Yarr! Our robo-pirates searched every corner of the digital seas, but the page has gone overboard! Maybe it walked the plank? Sail back to safer waters.
+
+              </Card.Text>
+
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
