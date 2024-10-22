70d4f6ce6f40835d3cebc0d7956ddc3a03b09f29
Potato <rmann0302@gmail.com>
2024-10-21 22:57:10 -0500

    add hero image video to index page and then tos to footer

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,4 +1,4 @@
-import Jokes from '@/components/Jokes';
+import HeroSection from '@/components/HeroImage';
 import React from 'react';
 import { Card, Col, Container, Row} from 'react-bootstrap';
 
@@ -6,18 +6,16 @@ const HomePage = () => {
 
   return (
     <>
-      <Container style={{ marginTop: '50px' }}>
-        <Row className="mb-4">
-          <Col md={{ offset: 3, span: 6 }}>
-            <Card bg="dark" text='white'>
-              <Card.Header>Home</Card.Header>
-              <Card.Img variant="top" src="/robot.png" alt="Home Page Card" width={300} height={200} style={{padding:'5px'}}/>
-              <Card.Body>                
-                <Jokes />
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          marginTop: '50px',
+          padding: 0,
+        }}>             
+        <HeroSection />
       </Container>
 
     </>
